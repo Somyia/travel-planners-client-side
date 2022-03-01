564b5372ed27ed9ba3994def112f1c94d2f42445
Somyia <somyiaakter93@gmail.com>
2022-03-02 03:47:16 +0600

    login-customization

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,6 +5,7 @@ import useAuth from '../../hooks/useAuth';
 import Banner from '../Home/Banner/Banner';
 import { useForm } from 'react-hook-form';
 import './Login.css';
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
     const { signInWithGoogle, setIsLoading } = useAuth();
@@ -29,25 +30,44 @@ const Login = () => {
 
             <Container className="py-5">
                 <Row>
-                    <Col md={{ span: 6, offset: 3 }}>
-                        <Card className="login-card">
-                            <div className="travel form login-form animate__animated animate__fadeInUp">
-                                <form>
-                                    <div>
+                    <Col md={{ span: 8, offset: 2 }} className="p-2">
+                        <div>
 
-                                        <input defaultValue="" placeholder="Your Name" {...register("name", { required: true })} />
+                            <Card className="login-card ">
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="col-md-5">
+                                        <img src="https://iili.io/E2P0wg.jpg" alt="" className="img-fluid" />
                                     </div>
-                                    <div>
-                                        <input placeholder="Email" type="email" defaultValue="" {...register("email", { required: true })} />
+                                    <div className="col-md-7 px-4 ">
+                                        <div className="travel form login-form animate__animated animate__fadeInUp">
+                                            <form>
+                                                <div>
 
+                                                    <input defaultValue="" placeholder="Your Name" {...register("name", { required: true })} />
+                                                </div>
+                                                <div>
+                                                    <input placeholder="Email" type="email" defaultValue="" {...register("email", { required: true })} />
+
+                                                </div>
+                                                <div>
+                                                    <input placeholder="Password" type="password" defaultValue="" {...register("password", { required: true })} />
+                                                </div>
+                                                <input className="travel-btn login-btn" type="submit" value="Register" />
+                                            </form>
+                                        </div>
+                                        <div className="text-center py-3">
+                                            <small>Or Continue With</small>
+                                            <p onClick={handleGoogleSignIn} style={{ fontSize: "25px" }}><FcGoogle /></p>
+                                        </div>
+                                        <div className="text-center">
+                                            <small>Already have an account?<button className="btn btn-link">Register</button></small>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <input placeholder="Password" type="password" defaultValue="" {...register("password", { required: true })} />
-                                    </div>
-                                    <input className="travel-btn login-btn" type="submit" value="Login" />
-                                </form>
-                            </div>
-                        </Card>
+                                </div>
+                            </Card>
+
+                        </div>
+
                     </Col>
                 </Row>
 
